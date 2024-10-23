d47738072b41bc27b18b9b51cd606aa03cef9e61
Dayane Rodrigues <dayane.rodrigues_silva@hotmail.com>
2024-10-22 22:25:52 -0300

    chore: criação da service e metodos findAll e findById

--- src/main/java/com/estudos/dayane/projetoweb/resources/UsuarioResource.java
+++ src/main/java/com/estudos/dayane/projetoweb/resources/UsuarioResource.java
@@ -1,18 +1,33 @@
 package com.estudos.dayane.projetoweb.resources;
 
 import com.estudos.dayane.projetoweb.entities.Usuario;
+import com.estudos.dayane.projetoweb.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
+    @Autowired
+    private UsuarioService service;
+
     @GetMapping
-    public ResponseEntity<Usuario> findAll(){
-        Usuario usuario = new Usuario(1L, "Dayane", "dayane@gmail.com","22222222", "123456");
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Usuario>> findById(@PathVariable Long id){
+        Optional<Usuario> usuario = service.findById(id);
         return ResponseEntity.ok().body(usuario);
     }
 
---
+++ src/main/java/com/estudos/dayane/projetoweb/services/UsuarioService.java
@@ -0,0 +1,23 @@
+package com.estudos.dayane.projetoweb.services;
+
+import com.estudos.dayane.projetoweb.entities.Usuario;
+import com.estudos.dayane.projetoweb.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+    public List<Usuario> findAll(){
+        return repository.findAll();
+    }
+
+    public Optional<Usuario> findById(Long id){
+        return repository.findById(id);
+    }
+}
