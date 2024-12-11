4d927a6d2db7851d243490a51801ee843509e3e5
Dayane Rodrigues <dayane.rodrigues_silva@hotmail.com>
2024-12-10 22:48:55 -0300

    chore: associação manyToMany, e tabela de associação

--- src/main/java/com/estudos/dayane/projetoweb/entities/Categoria.java
+++ src/main/java/com/estudos/dayane/projetoweb/entities/Categoria.java
@@ -1,5 +1,6 @@
 package com.estudos.dayane.projetoweb.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,7 +16,8 @@ public class Categoria implements Serializable {
     private Long id;
     private String nome;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categorias")
     private Set<Produto> produtos = new HashSet<>();
 
     public Categoria(){}
